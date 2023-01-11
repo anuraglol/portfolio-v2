d7b1ae1f32a5385409c2acdead3e829a96ab8af8
Anurag <kr.anurag24@gmail.com>
2023-01-11 11:52:56 +0530

    patch

--- src/lib/constants/ui.ts
+++ src/lib/constants/ui.ts
@@ -2,13 +2,15 @@ const DEFAULT_X_PADDING = {
   base: "6",
   sm: "12",
   md: "48",
-  lg: "96",
+  lg: "72",
   "2xl": "28rem",
 };
 
 const DEFAULT_FONT_SIZES = {
   base: "sm",
   sm: "md",
+  xl: "lg",
+  "2xl": "xl",
 };
 
 export { DEFAULT_X_PADDING, DEFAULT_FONT_SIZES };
